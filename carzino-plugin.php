f3383beccc3de5a10d9c04faf8ddf1d3ee2b9cb5
carzinoautos-setup <carzinoautos@gmail.com>
2025-09-05 14:37:36 -0700

    Add files via upload

---
+++ carzino-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Carzino Plugin
+ * Description: Loads the Carzino React App inside WordPress.
+ * Version: 1.0.0
+ * Author: Tony
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+require_once plugin_dir_path(__FILE__) . 'includes/enqueue.php';
