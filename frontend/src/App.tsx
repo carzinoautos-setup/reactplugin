d64e4df25f2ea873fc50ee82a7b71aa1eede68e6
carzinoautos-setup <carzinoautos@gmail.com>
2025-09-05 17:19:57 -0700

    Create App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Carzino React App</h1>
+      <p>This is your app running inside WordPress 🎉</p>
+    </div>
+  );
+}
+
+export default App;
