307f3dbd0bf905c8b30fdf9481107a7ba3011206
carzinoautos-setup <carzinoautos@gmail.com>
2025-09-05 17:34:41 -0700

    Create Header.tsx

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Header() {
+  return (
+    <header className="bg-blue-600 text-white p-4">
+      <h1 className="text-xl font-bold">Carzino</h1>
+    </header>
+  );
+}
